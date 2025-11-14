d77a6f5735ac5dc1a70451c909e97a19371a491c
George Honeywell <george.honeywell@live.co.uk>
2025-11-14 11:22:48 +0000

    Add project files.

---
+++ Chapter_01/AdditionCalculator/AdditionCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Welcome to the Addition Calculator!");
+Console.WriteLine("Enter your first whole number: ");
+bool firstNumber = int.TryParse(Console.ReadLine(), out int firstNum);
+if (!firstNumber)
+{
+  Console.WriteLine("The number you entered was not in the correct format!");
+  return;
+}
+
+Console.WriteLine("Enter your second whole number: ");
+bool secondNumber = int.TryParse(Console.ReadLine(), out int secondNum);
+if (!secondNumber)
+{
+  Console.WriteLine("The number you entered was not in the correct format!");
+  return;
+}
+
+int sum = firstNum + secondNum;
+
+Console.WriteLine($"{firstNum} + {secondNum} = {sum}");
