c76a6a474cdd7ddc72f07de3560fe3c9ac8561c6
George Honeywell <george.honeywell@live.co.uk>
2025-11-14 11:54:21 +0000

    Added another addition calculator which allows the uses of whole numbers and numbers with decimal points.

---
+++ Chapter_01/AdditionCalculatorWithDoubles/AdditionCalculatorWithDoubles/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Welcome to the Addition Calculator!");
+Console.WriteLine("Enter the first number: ");
+bool firstNumber = double.TryParse(Console.ReadLine(), out double firstNum);
+if(!firstNumber)
+{
+  Console.WriteLine("The number entered was not in the correct format!");
+  return;
+}
+
+Console.WriteLine("Enter the second number: ");
+bool secondNumber = double.TryParse(Console.ReadLine(), out double secondNum);
+if(!secondNumber)
+{
+  Console.WriteLine("The number entered was not in the correct format!");
+  return;
+}
+
+double sum = firstNum + secondNum;
+Console.WriteLine($"{firstNum} + {secondNum} = {sum}");
