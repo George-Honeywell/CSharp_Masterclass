e921b38980ddc742798488babe90de429e6a23be
George Honeywell <george.honeywell@live.co.uk>
2025-11-16 14:23:10 +0000

    Basic coding exercise using 'switch' statements.

---
+++ Chapter_02/SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿int day = 3;
+
+switch(day)
+{
+  case 1:
+    Console.WriteLine("Monday");
+    break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  case 4:
+    Console.WriteLine("Thursday");
+    break;
+  case 5:
+    Console.WriteLine("Friday");
+    break;
+  default:
+    // Default case is used whenever any of the cases are not executed.
+    Console.WriteLine("It's the weekend!");
+    break;
+}
