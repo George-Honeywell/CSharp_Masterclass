7d35ef244de84a50c6401023c7773f7a150db5fb
George Honeywell <george.honeywell@live.co.uk>
2025-11-16 16:37:46 +0000

    Created a basic 'Guess the Number' game.

---
+++ Chapter_02/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcome to guess the number!");
+Console.WriteLine("The rules are straight forward, guess the number!");
+Console.WriteLine("The number will be between 1 and 100. Good luck!");
+Console.Write("Enter your number: ");
+
+// Create an instance of the 'Random' class
+Random rand = new Random();
+
+// The 'maxValue' isn't returned, so setting the maxValue to 101 ensures the random generation is between 1 and 100
+int randomNumber = rand.Next(1, 101);
+string guess = Console.ReadLine();
+
+bool isValidNumber = int.TryParse(guess, out int res);
+if(isValidNumber)
+{
+  if (res == randomNumber)
+    Console.WriteLine("You guessed coprrectly!");
+  else
+  {
+    Console.WriteLine("Incorrect guess!");
+    Console.WriteLine($"The correct number was {randomNumber}");
+  }
+}
+else
+  Console.WriteLine("Invalid Input - Quitting...");
---
+++ Chapter_02/test/test/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = 125;
+
+
+if (number % 5 == 0)
+  Console.WriteLine($"{number} is a multiple of 5.");
+else
+  Console.WriteLine($"{number} is not a multiple of 5.");
