a22a681c4c89d4b4515539abf55c8facaeda41fa
George Honeywell <george.honeywell@live.co.uk>
2025-11-17 11:56:38 +0000

    Cleaned up alot of repetitive code.

--- Chapter_02/ConditionalBasedCalculator/ConditionalBasedCalculator/Program.cs
+++ Chapter_02/ConditionalBasedCalculator/ConditionalBasedCalculator/Program.cs
@@ -1,98 +1,45 @@
-﻿Console.Write("What operation would you like to perform on 2 numbers? (+, -, *, /): ");
-string op = Console.ReadLine();
-
-double firstNum = 0;
+﻿double firstNum = 0;
 double secondNum = 0;
 double result = 0;
 bool isValid = false;
 
+Console.Write("Enter your first number: ");
+isValid = double.TryParse(Console.ReadLine(), out firstNum);
+if (!isValid)
+{
+  Console.WriteLine("Incorrect Value.");
+  return;
+}
+
+Console.Write("Enter your second number: ");
+isValid = double.TryParse(Console.ReadLine(), out secondNum);
+if (!isValid)
+{
+  Console.WriteLine("Incorrect Value.");
+  return;
+}
+
+Console.Write("What operation would you like to perform on the 2 numbers? (+, -, *, /): ");
+string op = Console.ReadLine();
+
 switch(op)
 {
-  case "+":
-    Console.WriteLine("You've chosen to add 2 numbers.");
-    
-    Console.Write("Enter your first number: ");
-    isValid = double.TryParse(Console.ReadLine(), out firstNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
-
-    Console.Write("\nEnter your second number: ");
-    isValid = double.TryParse(Console.ReadLine(), out secondNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
+  case "+":   
     result = firstNum + secondNum;
     Console.WriteLine($"{firstNum} + {secondNum} = {result}");
     break;
 
   case "-":
-    Console.WriteLine("You've chosen to subtract 2 numbers.");
-
-    Console.Write("Enter your first number: ");
-    isValid = double.TryParse(Console.ReadLine(), out firstNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
-
-    Console.Write("\nEnter your second number: ");
-    isValid = double.TryParse(Console.ReadLine(), out secondNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
     result = firstNum - secondNum;
     Console.WriteLine($"{firstNum} - {secondNum} = {result}");
     break;
 
   case "*":
-    Console.WriteLine("You've chosen to multiply 2 numbers.");
-
-    Console.Write("Enter your first number: ");
-    isValid = double.TryParse(Console.ReadLine(), out firstNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
-
-    Console.Write("\nEnter your second number: ");
-    isValid = double.TryParse(Console.ReadLine(), out secondNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
     result = firstNum * secondNum;
     Console.WriteLine($"{firstNum} * {secondNum} = {result}");
     break;
 
   case "/":
-    Console.WriteLine("You've chosen to divide 2 numbers.");
-
-    Console.Write("Enter your first number: ");
-    isValid = double.TryParse(Console.ReadLine(), out firstNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
-
-    Console.Write("\nEnter your second number: ");
-    isValid = double.TryParse(Console.ReadLine(), out secondNum);
-    if (!isValid)
-    {
-      Console.WriteLine("Incorrect Value.");
-      return;
-    }
-
     if(secondNum == 0)
     {
       Console.WriteLine("Unable to divide by zero!");
