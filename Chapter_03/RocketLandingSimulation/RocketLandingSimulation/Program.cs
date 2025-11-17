efe12eff75329c09a30a8c39609ea484c23aa286
George Honeywell <george.honeywell@live.co.uk>
2025-11-17 13:19:43 +0000

    Simple project to make a rocket descend using 'for' loops.

---
+++ Chapter_03/RocketLandingSimulation/RocketLandingSimulation/Program.cs
@@ -0,0 +1,30 @@
+﻿for(int i = 0; i < 10; i++)
+{
+  Console.Clear();
+  for (int j = 0; j < i; j++)
+  {
+    Console.WriteLine(" ");
+  }
+
+  Console.WriteLine("    ___   ");
+  Console.WriteLine("   /   \\  ");
+  Console.WriteLine("  /     \\ ");
+  Console.WriteLine(" /_______\\");
+  Console.WriteLine(" |       | ");
+  Console.WriteLine(" |       | ");
+  Console.WriteLine(" |   _   | ");
+  Console.WriteLine(" |  | |  | ");
+  Console.WriteLine(" |  | |  | ");
+  Console.WriteLine(" |  |_|  | ");
+  Console.WriteLine(" |       | ");
+  Console.WriteLine(" |_______| ");
+  Console.WriteLine("   \\/ \\/ ");
+  Thread.Sleep(500);
+
+}
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine("------- LANDING       PAD -------");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("The rocket has landed! Good job!");
+
