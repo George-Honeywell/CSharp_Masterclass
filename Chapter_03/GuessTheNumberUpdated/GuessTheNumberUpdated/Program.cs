9fd9aecabf6a4dbccba7f8d3f0cec53b5161da75
George Honeywell <george.honeywell@live.co.uk>
2025-11-17 14:36:16 +0000

    Recreated Guess The Number game, using a while loop allowing for continious play.

---
+++ Chapter_03/GuessTheNumberUpdated/GuessTheNumberUpdated/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Welcome to guess the number!");
+Console.WriteLine("The rules are straight forward, guess the number!");
+Console.WriteLine("The number will be between 1 and 100. Good luck!");
+
+Random rand = new Random();
+
+int randomNumber = rand.Next(1, 101);
+int res = 0;
+int numOfGuesses = 0;
+
+while (res != randomNumber)
+{
+  Console.Write("Enter your number: ");
+  string guess = Console.ReadLine();
+  bool isValidNumber = int.TryParse(guess, out res);
+
+  if (!isValidNumber)
+  {
+    Console.WriteLine("Invalid Number...");
+    continue;
+  }
+
+  if (res != randomNumber)
+  {
+    Console.WriteLine("Incorrect guess, try again!");
+    numOfGuesses++;
+  }
+  
+  if (res < randomNumber)
+    Console.WriteLine("The number I have in mind is higher!");
+  else if (res > randomNumber)
+    Console.WriteLine("The number I have in mind is lower!");
+}
+
+Console.WriteLine($"You guessed correctly! It took you {numOfGuesses} guesses!");
