086d2b532cb68ff59f60fa70b240638d726ca837
George Honeywell <george.honeywell@live.co.uk>
2025-11-18 11:31:03 +0000

    A small challenge to add the sum of each row of a 2d array.

---
+++ Chapter_03/ArraySumChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+int sumRowOne = 0;
+int sumRowTwo = 0;
+int sumRowThree = 0;
+
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+  sumRowOne += numbers[0, i];
+  sumRowTwo += numbers[1, i];
+  sumRowThree += numbers[2, i];
+}
+
+Console.WriteLine($"The Sum of Row 1 is {sumRowOne}.");
+Console.WriteLine($"The Sum of Row 2 is {sumRowTwo}.");
+Console.WriteLine($"The Sum of Row 3 is {sumRowThree}.");
