cfcde4099bf45672eb9e26e68b57e932b7b716dd
George Honeywell <george.honeywell@live.co.uk>
2025-11-21 13:20:53 +0000

    Introduction to field / instances variables, using top-level statements.

---
+++ Chapter_04/FieldsAndInstanceVariables/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FieldsAndInstanceVariables
+{
+  internal class Program
+  {
+    // As the variable is declared in the class and not a method, it's a 'field'
+    // This can be used throughout the class
+    static int returnValue;
+
+    static void Main(string[] args)
+    {
+      returnValue = Add(5, 5);
+      Console.WriteLine($"5 + 5 = {returnValue}");
+
+      returnValue = Subtract(5, 5);
+      Console.WriteLine($"5 - 5 = {returnValue}");
+    }
+
+    static int Add(int a, int b)
+    {
+      return a + b;
+    }
+
+    static int Subtract(int a, int b)
+    {
+      return a - b;
+    }
+  }
+}
