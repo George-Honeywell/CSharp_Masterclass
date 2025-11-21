c44a84690f8972eeebf5177f6b90913854deea40
George Honeywell <george.honeywell@live.co.uk>
2025-11-21 15:40:30 +0000

    A basic weather simulator app.

---
+++ Chapter_04/BasicWeatherSimulator/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace BasicWeatherSimulator
+{
+  internal class Program
+  {
+    static int[] temperature;
+    static string[] conditions = { "Clear", "Cloudy", "Overcast", "Light Rain", "Heavy Rain", "Foggy", "Snowy" };
+    static string[] weatherConditions;
+
+    static void Main(string[] args)
+    {
+      Console.Write("Enter the number of days to simulate: ");
+      int days = int.Parse(Console.ReadLine());
+
+      temperature = new int[days];
+      weatherConditions = new string[days];
+
+      Random rand = new Random();
+
+      for(int i = 0; i < days; i++)
+      {
+        temperature[i] = rand.Next(-10, 36);
+        weatherConditions[i] = conditions[rand.Next(conditions.Length)];
+      }
+
+      Console.WriteLine($"The highest temperature is: {GetMaxTemperature()}");     
+      Console.WriteLine($"The lowest temperature is: {GetLowestTemperature()}");
+      Console.WriteLine($"The average temperature is: {GetAverageTemp()}");
+      Console.WriteLine($"The most common weather condition is: {GetMostCommonWeatherCondition(weatherConditions)}");
+    }
+
+    static int GetMaxTemperature()
+    {
+      return temperature.Max();
+    }
+
+    static int GetLowestTemperature()
+    {
+      return temperature.Min(); 
+    }
+
+    static double GetAverageTemp()
+    { 
+      return Math.Round(temperature.Average(), 2); 
+    }
+
+    static string GetMostCommonWeatherCondition(string[] conditions)
+    {
+      // TODO
+      // - Include code to return 'No common condition' if each condition only appears once
+      // - Include 'multiple' conditions if the same (or more) condition occurs the same number of times
+
+      int count = 0;
+      string mostCommon = conditions[0];
+
+      for (int i = 0; i < conditions.Length; i++)
+      {
+        int tempCount = 0;
+
+        for(int j = 0; j < conditions.Length; j++)
+        {
+          if (conditions[j] == conditions[i])
+          {
+            tempCount++;
+          }
+        }
+
+        if(tempCount > count)
+        {
+          count = tempCount;
+          mostCommon = conditions[i];
+        }
+      }
+
+      return mostCommon;
+    }
+  }
+}
