f9d7c6472e181f6ebf1a61e26f6c2c9559746678
George Honeywell <george.honeywell@live.co.uk>
2025-11-24 14:06:01 +0000

    Example of using multiple constructors in a class.

---
+++ Chapter_05/MultipleConstructors/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleConstructors
+{
+  internal class Customer
+  {
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public int ContactNumber { get; set; }
+
+    // Default Constructor
+    public Customer()
+    {
+      Name = "<No Name Set>";
+      Country = "<No Country Set>";
+      ContactNumber = 0;
+    }
+
+    // Custom Constructor
+    public Customer(string name, string country, int contactNumber)
+    {
+      Name = name;
+      Country = country;
+      ContactNumber = contactNumber;
+    }
+
+    public Customer(string name)
+    {
+      Name = name;
+    }
+  }
+}
---
+++ Chapter_05/MultipleConstructors/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MultipleConstructors
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      // Uses the Constructor which accepts 3 arguments
+      Customer george = new Customer("George", "United Kingdom", 123456789);
+      Console.WriteLine("--- CUSTOMER DETAILS ---");
+      Console.WriteLine($"\tName: {george.Name}\n\tCountry: {george.Country}\n\tContact Number: {george.ContactNumber}");
+
+      // Uses the Constructor which only accepts 1 argument
+      Customer jack = new Customer("Jack");
+      Console.WriteLine("--- CUSTOMER DETAILS ---");
+      Console.WriteLine($"\tName: {jack.Name}\n\tCountry: {jack.Country}\n\tContact Number: {jack.ContactNumber}");
+
+      // Uses the Default Constructor with no arguments required
+      Customer andy = new Customer();
+      Console.WriteLine("--- CUSTOMER DETAILS ---");
+      Console.WriteLine($"\tName: {andy.Name}\n\tCountry: {andy.Country}\n\tContact Number: {andy.ContactNumber}");
+
+    }
+  }
+}
