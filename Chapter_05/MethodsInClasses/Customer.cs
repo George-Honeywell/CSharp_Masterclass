d26d1c06a99f2859341f6cc182b13d4667cc112c
George Honeywell <george.honeywell@live.co.uk>
2025-11-24 14:31:07 +0000

    Introduction into using Methods within classes.

---
+++ Chapter_05/MethodsInClasses/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsInClasses
+{
+  internal class Customer
+  {
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public int ContactNumber { get; set; }
+
+    private static int Count = 0;
+
+    // Default Constructor
+    public Customer()
+    {
+      Name = "<No Name Set>";
+      Country = "<No Country Set>";
+      ContactNumber = 0;
+      Count++;
+    }
+
+    // Custom Constructor
+    public Customer(string name, string country, int contactNumber)
+    {
+      Name = name;
+      Country = country;
+      ContactNumber = contactNumber;
+      Count++;
+    }
+
+    public Customer(string name)
+    {
+      Name = name;
+    }
+
+    // Defined methods within the class which allow the class to display information
+    // Methods can be used to change the state of a class too
+    // This method can get the information for EACH instance of the class
+    public void GetCustomerDetails()
+    {
+      Console.WriteLine("--- CUSTOMER DETAILS ---");
+      Console.WriteLine($"\tName: {Name}\n\tCountry: {Country}\n\tContact Number: {ContactNumber}\n");
+    }
+  }
+}
---
+++ Chapter_05/MethodsInClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MethodsInClasses
+{
+  internal class Program
+  {
+    public static void Main()
+    {
+      // Uses the Constructor which accepts 3 arguments
+      Customer george = new Customer("George", "United Kingdom", 123456789);
+      george.GetCustomerDetails();
+
+      // Uses the Constructor which only accepts 1 argument
+      Customer jack = new Customer("Jack");
+      jack.GetCustomerDetails();
+
+      // Uses the Default Constructor with no arguments required
+      Customer andy = new Customer();
+      andy.GetCustomerDetails();
+    }
+  }
+}
