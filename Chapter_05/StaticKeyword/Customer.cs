ba0127f895120b222cb5bfd7ffe62e01b3133715
George Honeywell <george.honeywell@live.co.uk>
2025-12-01 15:53:29 +0000

    Introduction to the 'static' keyword for methods and how they're used within classes.

---
+++ Chapter_05/StaticKeyword/Customer.cs
@@ -0,0 +1,42 @@
+﻿namespace StaticKeyword
+{
+  internal class Customer
+  {
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public int ContactNumber { get; set; }
+
+    // Default Constructor
+    public Customer()
+    {
+      Name = "<No Name Set>";
+      Country = "<No Country Set>";
+      ContactNumber = 0;
+    }
+
+    // Custom Constructor
+    public Customer(string name, string country, int contactNumber)
+    {
+      Name = name;
+      Country = country;
+      ContactNumber = contactNumber;
+    }
+
+    public Customer(string name)
+    {
+      Name = name;
+    }
+
+    public void SetCustomerDetails(string name, string country, int contactNum)
+    {
+      Name = name;
+      Country = country;
+      ContactNumber = contactNum;
+    }
+
+    public static void GetCustomerCount()
+    {
+      Console.WriteLine("This method is static and therefore can be called without being created by an instance of the Customer object.");
+    }
+  }
+}
---
+++ Chapter_05/StaticKeyword/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StaticKeyword
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // The 'SetCustomerDetails' method can only be called on an object instance of 'Customer' as it's not marked as static
+      Customer customer = new Customer();
+      customer.SetCustomerDetails("George", "United Kingdom", 123456789);
+
+      // If a method is marked as 'static', the method must be called on the class, not an instance
+      // As 'GetCustomerCount()' is marked as static, it can be called from the class directly
+      // Note: Any methods marked as static don't belong to a single instance of a class
+      // Attempting to call 'GetCustomerCount()' from an object instance will result in a compile-time error
+      Customer.GetCustomerCount();
+    }
+  }
+}
