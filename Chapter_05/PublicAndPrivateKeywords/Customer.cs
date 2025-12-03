04601f703ea6a708fd75df50a71d789ad92a50bc
George Honeywell <george.honeywell@live.co.uk>
2025-12-03 11:49:02 +0000

    Introduction to the 'private' and 'public' access modifiers.

---
+++ Chapter_05/PublicAndPrivateKeywords/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace PublicAndPrivateKeywords
+{
+  // Access Modifiers
+  // public - Any method, class, field, variable etc. marked as 'public' can be accessed in any class
+  // private - Any method, class, field, variable etc. marked as 'private' can only be accessed within the class it's defined in
+  internal class Customer
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Country { get; set; }
+    // Age will not be able to be accessed in any other class.
+    private int _Age { get; set; }
+
+    public Customer()
+    {
+      Console.WriteLine("new customer created with no information. Ensure information is added as soon as possible.");
+    }
+
+    private string GetCustomerName()
+    {
+      return FirstName + " " + LastName;
+    }
+  }
+}
---
+++ Chapter_05/PublicAndPrivateKeywords/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PublicAndPrivateKeywords
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Customer ghoneywell = new Customer();
+
+      // Using the public fields to set values for the new customer
+      ghoneywell.FirstName = "George";
+      ghoneywell.LastName = "Honeywell";
+      ghoneywell.Country = "United Kingdom";
+
+      // As '_Age' is marked as private, it cannot be accessed here
+      // Inaccessible due to it's protection level
+      // ghoneywell._Age = 32;
+
+      // The same applies to methods too
+      // Inaccessible due to it's protection level
+      // ghoneywell.GetCustomerName();
+    }
+  }
+}
