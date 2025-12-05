7546a1e339fe7e84a908ff7d6d5ca381faadc336
George Honeywell <george.honeywell@live.co.uk>
2025-12-05 10:22:33 +0000

    Coding exercise to practice read-only properties.

---
+++ Chapter_05/CE_ReadonlyProperties/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CE_ReadonlyProperties
+{
+  public class Exercise
+  {
+    // Create a 'private readonly string'
+    private readonly string _message = "";
+    // Create a public read only property, which is defined by the 'get' with no 'set' available
+    public string Message { get { return _message; } }
+
+    // In the constructor, set the value of the 'readonly' field to whatever is passed into the constructor
+    public Exercise(string val)
+    {
+      _message = val; 
+    }
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Exercise ex = new Exercise("This is a 'Read-Only' Property!");
+      Console.WriteLine(ex.Message);
+    }
+  }
+}
