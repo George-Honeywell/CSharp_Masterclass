4d4d4f208111c831dc503c9680094aa82f65c121
George Honeywell <george.honeywell@live.co.uk>
2025-12-05 12:01:02 +0000

    Implementation of the Questions class

--- Chapter_05/PROJ_QuizApp/Questions.cs
+++ Chapter_05/PROJ_QuizApp/Questions.cs
@@ -2,6 +2,20 @@
 {
   internal class Questions
   {
-    
+    public string Question { get; set; }
+    public string[] Answers { get; set; }
+    public int AnswerIndex { get; set; }
+
+    public Questions(string question, string[] answers, int answerIndex)
+    {
+      Question = question;
+      Answers = answers;
+      AnswerIndex = answerIndex;
+    }
+
+    public bool IsCorrect(int choice)
+    {
+      return AnswerIndex == choice;
+    }
   }
 }
