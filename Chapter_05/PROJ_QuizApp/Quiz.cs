084042f1a48051233ccdcbed03d462ef03d9af00
George Honeywell <george.honeywell@live.co.uk>
2025-12-05 13:19:06 +0000

    Added an explanation of using 'this' keyword and how its used within a class.

---
+++ Chapter_05/PROJ_QuizApp/Quiz.cs
@@ -0,0 +1,15 @@
+﻿namespace PROJ_QuizApp
+{
+  internal class Quiz
+  {
+    private Questions[] questions;
+
+    public Quiz(Questions[] questions)
+    {
+      // The 'this' keyword refers to the current instance of a class
+      // 'this.questions' explicitly refers to the instance field, while 'questions' alone refers to the methods parameter.
+      // Without using 'this', the compiler assumes the parameter is being used, not the field
+      this.questions = questions;
+    }
+  }
+}
