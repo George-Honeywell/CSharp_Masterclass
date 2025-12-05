7425e767c67e9ff80718f627a6df13cdcb8b60ac
George Honeywell <george.honeywell@live.co.uk>
2025-12-05 11:05:39 +0000

    Introducion to Write Only properties.

---
+++ Chapter_05/WriteOnlyProperties/Customer.cs
@@ -0,0 +1,36 @@
+﻿namespace WriteOnlyProperties
+{
+  internal class Customer
+  {
+    private static int nextId = 0;
+    private readonly int _id;
+    private string _password;
+    // Write only Property
+    public string Password { set { _password = value; } }
+    public int Id { get { return _id; } }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Country { get; set; }
+
+    public Customer(string firstName, string lastName, string country = "<Not Set>")
+    {
+      _id = nextId++;
+      FirstName = firstName;
+      LastName = lastName;
+      Country = country;
+    }
+
+    public Customer(string firstName, string lastName)
+    {
+      _id = nextId++;
+      FirstName = firstName;
+      LastName = lastName;
+    }
+
+    public void GetCustomerId()
+    {
+      Console.WriteLine($"The customers unique ID is: {_id}");
+    }
+  }
+}
---
+++ Chapter_05/WriteOnlyProperties/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WriteOnlyProperties
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Customer ghoneywell = new Customer("George", "Honeywell");
+      ghoneywell.GetCustomerId();
+
+      Customer atomline = new Customer("Andy", "Tomline");
+      atomline.GetCustomerId();
+
+      // As this property only contains 'set', this property can only have it's value set
+      atomline.Password = "qwerty";
+      // Console.WriteLine(atomline.Password); // CS0154 : Property does not contain an accessor 'get' and cannot be used in this context
+    }
+  }
+}
