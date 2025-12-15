7633355ec6e03615154340b88ceea2ca8cab36af
George Honeywell <george.honeywell@live.co.uk>
2025-12-15 11:26:57 +0000

    Added an example of using 'Where', basics of using LINQ.

--- Chapter_06/ListsWithComplexObjects/Program.cs
+++ Chapter_06/ListsWithComplexObjects/Program.cs
@@ -4,11 +4,13 @@
   {
     public string Name { get; set; }
     public int MaxWeightKg { get; set; }
+    public string Area { get; set; }
 
-    public Exercises(string name, int maxWeightKg)
+    public Exercises(string name, int maxWeightKg, string area)
     {
       Name = name;
       MaxWeightKg = maxWeightKg;
+      Area = area;
     }
 
     public Exercises() { }
@@ -20,10 +22,16 @@
     static void Main(string[] args)
     {
       // Adding new items if a constructor exists
-      exercises.Add(new Exercises ( "Bench Press", 60 ));
+      exercises.Add(new Exercises("Bench Press", 60, "Chest"));
+      exercises.Add(new Exercises("Chest Flys", 50, "Chest"));
+      exercises.Add(new Exercises("Cable Bicep Curl", 30, "Arms"));
+      exercises.Add(new Exercises("Cable Hammer Curl", 60, "Arms"));
+      exercises.Add(new Exercises("Hack Squat", 60, "Legs"));
+      exercises.Add(new Exercises("Smith Machine Bulgarian Split Squat", 53, "Legs"));
+      exercises.Add(new Exercises("Dumbbell Lateral Raise", 10, "Back"));
 
       // Adding new items if a constructur doesn't exist
-      exercises.Add(new Exercises { Name = "Barbell Back Squat", MaxWeightKg = 100 });
+      exercises.Add(new Exercises { Name = "Barbell Back Squat", MaxWeightKg = 100, Area = "Legs" });
 
       // Test adding a custom exercise
       Console.Write("Enter a name for a exercise: ");
@@ -31,7 +39,11 @@
 
       Console.Write("Enter the maximum weight used for the exercise (kg): ");
       int exerciseWeight = Convert.ToInt32(Console.ReadLine());
-      AddExercise(exerciseName, exerciseWeight);
+
+      Console.Write("Enter the area of the body this exercise trains: ");
+      string exerciseArea = Console.ReadLine();
+      
+      AddExercise(exerciseName, exerciseWeight, exerciseArea);
 
       // Print out the contents of the List
       Console.WriteLine("Exercises in Database: ");
@@ -39,11 +51,21 @@
       {
         Console.WriteLine($"Name: {exercise.Name} - Max Weight (kg): {exercise.MaxWeightKg}");
       }
+
+      // Populates this list where the exercises are focused on the Chest
+      List<Exercises> chestExercises = exercises.Where(e => e.Area == "Chest").ToList();
+      Console.WriteLine("Exercises that focus on the Chest: ");
+      int count = 1;
+      foreach(var chest in chestExercises)
+      {
+        Console.WriteLine($"{count}. Name: {chest.Name} - Max Weight (kg): {chest.MaxWeightKg}");
+        count++;
+      }
     }
 
-    public static void AddExercise(string name, int maxWeightKg)
+    public static void AddExercise(string name, int maxWeightKg, string area)
     {
-      exercises.Add(new Exercises(name, maxWeightKg));
+      exercises.Add(new Exercises(name, maxWeightKg, area));
     }
   }
 }
